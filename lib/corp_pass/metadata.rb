fe2126bef265e9a71b3676c0a02e2c9b6b3bc4be
yanyee wong <wong_yan_yee@tech.gov.sg>
2020-02-17 19:33:32 +0800

    used Xmldsig::XSD_X509_SERIAL_FIX_FILE schema to allow string types for X509SerialNumber

--- lib/corp_pass/metadata.rb
+++ lib/corp_pass/metadata.rb
@@ -24,7 +24,7 @@ module CorpPass
 
     def self.verify_signature(signed_xml, signing_certificate)
       signed_document = Xmldsig::SignedDocument.new(signed_xml)
-      signed_document.validate do |signature, data, signature_algorithm|
+      signed_document.validate(schema: Xmldsig::XSD_X509_SERIAL_FIX_FILE) do |signature, data, signature_algorithm|
         signing_certificate.public_key.verify(digest_method(signature_algorithm).new,
                                               signature, data)
       end
